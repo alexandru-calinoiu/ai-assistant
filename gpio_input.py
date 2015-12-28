4d9fa6a8691d33f71dfbdafc689fa6192a1792ac
alexandru-calinoiu <calinoiu.alexandru@agilefreaks.com>
2015-12-28 16:43:48 +0200

    read using callback

--- gpio_input.py
+++ gpio_input.py
@@ -5,10 +5,17 @@ gpio_pin = 4
 pi.set_mode(gpio_pin, pigpio.INPUT)
 pi.set_pull_up_down(gpio_pin, pigpio.PUD_DOWN)
 
-last_value = -1
-while True:
-    value = pi.read(gpio_pin)
 
-    if value != last_value:
-        last_value = value
-        print(value)
+def rising_callback(gpio, level, tick):
+    print('Rising: ', gpio, level, tick)
+
+
+def failing_callback(gpio, level, tick):
+    print('Failing: ', gpio, level, tick)
+
+
+pi.callback(gpio_pin, pigpio.RISING_EDGE, rising_callback)
+pi.callback(gpio_pin, pigpio.FALLING_EDGE, failing_callback)
+
+while True:
+    continue
