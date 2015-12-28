93b69c5b1bf0365832e1ee8854090902886e577c
alexandru-calinoiu <calinoiu.alexandru@agilefreaks.com>
2015-12-28 16:07:39 +0200

    adds gpio console

---
+++ gpio_17.py
@@ -0,0 +1,16 @@
+import pigpio
+
+pi = pigpio.pi()
+
+gpio_pin = 14
+pi.set_mode(gpio_pin, pigpio.OUTPUT)
+
+while True:
+    answer = input('What do you want to do: ')
+
+    if answer == 'on':
+        pi.write(gpio_pin, 1)
+    elif answer == 'off':
+        pi.write(gpio_pin, 0)
+    elif answer == 'exit':
+        break
